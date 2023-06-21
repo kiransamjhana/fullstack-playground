153a942991705e5e2f45bac46925111a45697365
kiransamjhana <kiranmanandhar956@gmail.com>
2023-06-21 17:38:37 +1000

    backend play

---
+++ backend-play/server.js
@@ -0,0 +1,27 @@
+//importing the required module
+
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+//assigning the port number to the server
+const PORT = 8090;
+
+///using midddleware
+app.use(express.json());
+// listening server to the port
+app.listen(PORT, (error) => {
+  error && console.log(error.message);
+  console.log("your server is running at http://localhost:${PORT}");
+});
+
+//get methods
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "sucess",
+    message: "your server is running healthy",
+  });
+});
