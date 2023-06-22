05e76fd0be4e7fa5236fd44c27906130962225ea
kiransamjhana <kiranmanandhar956@gmail.com>
2023-06-22 20:16:08 +1000

    new file

--- frontend-react/src/Components/Display.js
+++ frontend-react/src/Components/Display.js
@@ -7,72 +7,9 @@ export const Display = () => {
     <div>
       <Row className="mt-5">
         <Col>
-          <h3 className="text-center">Entry List</h3>
+          <h2> TaskEntry</h2>
           <hr />
-          <Table striped bordered hover variant="dark">
-            <tbody>
-              {entryList.map((item, i) => (
-                <tr key={i}>
-                  <td>{i + 1}</td>
-                  <td>
-                    <div className="d-flex">
-                      <Form.Check value={item._id} onChange={handleOnSelect} />{" "}
-                      {item.task}
-                    </div>
-                  </td>
-
-                  <td>{item.hr}</td>
-                  <td>
-                    <Button
-                      variant="info"
-                      title="Mark as bad list"
-                      onClick={() =>
-                        handleOnSwitch({ _id: item._id, type: "bad" })
-                      }
-                    >
-                      <i class="fa-solid fa-arrow-right"></i>
-                    </Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
-        <Col>
-          <h3 className="text-center">Bad List</h3>
-          <hr />
-          <Table striped bordered hover variant="dark">
-            <tbody>
-              {badList.map((item, i) => (
-                <tr key={i}>
-                  <td>{i + 1}</td>
-                  <td>
-                    <div className="d-flex">
-                      <Form.Check value={item._id} onChange={handleOnSelect} />{" "}
-                      {item.task}
-                    </div>
-                  </td>
-                  <td>{item.hr}</td>
-                  <td>
-                    <Button
-                      variant="danger"
-                      title="Mark as bad entry"
-                      onClick={() =>
-                        handleOnSwitch({ _id: item._id, type: "entry" })
-                      }
-                    >
-                      <i class="fa-solid fa-arrow-left"></i>
-                    </Button>
-                  </td>
-                </tr>
-              ))}
-
-              <tr>
-                <td colSpan={3}>You could gave saved</td>
-                <td>{badTotal}hr</td>
-              </tr>
-            </tbody>
-          </Table>
+          <Table striped bordered hover variant="dark"></Table>
         </Col>
       </Row>
     </div>
--- frontend-react/src/Components/InputForm.js
+++ frontend-react/src/Components/InputForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
@@ -6,17 +6,32 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 
 export const InputForm = () => {
+  const [form, setForm] = useState({});
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    console.log(setForm);
+  };
+
   return (
     <div>
       <h1> Track Your Activity</h1>
-      <Form className=" border p-2 bg-light rounded">
+      <Form className=" border p-2 bg-light rounded" onSubmit={handleOnSubmit}>
         <Row className="g-2">
           <Col md="6">
             <Form.Control
               required
               className="mb-2"
               id="inlineFormInput"
+              name="task"
               placeholder=" Coding"
+              onChange={handleOnChange}
             />
           </Col>
           <Col md="2">
@@ -26,6 +41,8 @@ export const InputForm = () => {
                 id="inlineFormInputGroup"
                 placeholder="3"
                 type="number"
+                name="hr"
+                onChange={handleOnChange}
               />
             </InputGroup>
           </Col>
